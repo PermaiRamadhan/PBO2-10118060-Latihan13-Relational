68a064de8c9f43bbf5261815f109782694e66bd7
Synxsaints <Synxsaints@LAPTOP-EAMMNJ87>
2019-09-26 15:14:53 +0700

    First Commit

---
+++ src/relational/Relational.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package relational;
+
+/**
+ *
+ * @author 
+ * Nama : Permai Ramadhan
+ * Kelas : IF 2
+ * NIM : 10118060
+ * Deskripsi Program : Relational
+ */
+public class Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("a >= b = " + (bil1 >= bil2));
+        System.out.println("a <= b = " + (bil1 <= bil2));
+    }
+    
+}
